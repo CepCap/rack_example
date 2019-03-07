9446a13cfd586c8eb94e23c9dc9ccabe68157297
CepCap <nikinoob@gmail.com>
2019-03-07 12:27:30 -0800

    Major corrections

--- rack/app.rb
+++ rack/app.rb
@@ -1,6 +1,32 @@
+require_relative 'helper/time_format'
+
 class App
 
   def call(env)
+    request = Rack::Request.new(env)
+    response = Rack::Response.new
+    response['Content-Type'] = 'text/plain'
+    time_format(request.params, response)
+    response.finish
+  end
+
+  private
+
+  def time_format(params, response)
+    if params.has_key?('format')
+      format = TimeFormat.new
+      format.call(params['format'])
+      if format.wrong_format?
+        response.status = 400
+        response.body = [format.formatted_time]
+      else
+        response.status = 200
+        response.body = [format.formatted_time]
+      end
+    else
+      response.status = 404
+      response.body = ['Not Found']
+    end
   end
 
 end
--- rack/config.ru
+++ rack/config.ru
@@ -1,5 +1,3 @@
-require_relative 'middleware/time_format'
 require_relative 'app'
 
-use TimeFormat
 run App.new
